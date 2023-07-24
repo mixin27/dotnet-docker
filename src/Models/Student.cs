8ca74dafdc481c6ba4b5465c7dcdae23ff42bfc4
Kyaw Zayar Tun <kyawzayartun.dev@gmail.com>
2023-07-24 11:03:48 +0630

    initial commit

---
+++ src/Models/Student.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace myWebApp.Models;
+
+public class Student
+{
+    public int Id {get; set;}
+    public string LastName {get; set;}
+    public string FirstMidName {get; set;}
+    public DateTime EnrollmentDate {get; set;}
+}
